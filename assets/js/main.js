6e604c377fd39e2e48b01c90ac2b1f03cd63a666
Mateusz Kubaszek <mkubasz@gmail.com>
2024-10-23 23:00:07 +0200

    feat: ludziki

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("November 27, 2024").getTime();
+var target_date = new Date(2024, 11, 27, 16, 0, 0).getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
