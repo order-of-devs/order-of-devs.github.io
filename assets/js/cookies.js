b6230ee0641147bf42faed94fd3fcd45e73c1c0d
Lena Sędkiewicz <91282230+lenasedkiewicz@users.noreply.github.com>
2024-11-24 19:49:35 +0100

    Feature/cookie banner library (#22)
    * Add basic cookie banner
    
    * Adjust styling
    
    * Update cookie banner styling
    
    * Add cookies rejection
    
    * Fix naming
    
    * Add basic cookie code
    
    * Add cookie banner
    
    * Add condition for google tag
    
    * Fix condition
    
    ---------
    
    Co-authored-by: Lena Sędkiewicz <91282230+msedkiewicz@users.noreply.github.com>

---
+++ assets/js/cookies.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const setCookie = (name, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+};
+const getCookie = (name) => {
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split("=");
+    if (cookieName === name) return cookieValue;
+  }
+  return null;
+};
+
+const handleCookieChoice = (choice) => {
+  if (choice === "accept") {
+    setCookie("userConsent", "accept", 365);
+  } else if (choice === "reject") {
+    setCookie("userConsent", "reject", 365);
+    const cookiesToDelete = ["_ga", "_ga_J03H31BQKV"];
+
+    cookiesToDelete.forEach((cookieName) => {
+      document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    });
+  }
+
+  document.getElementById("cookie-banner").classList.add("d-none");
+};
+
+window.addEventListener("load", () => {
+  if (!getCookie("userConsent")) {
+    document.getElementById("cookie-banner").classList.remove("d-none");
+  }
+});
+
+document
+  .getElementById("accept-cookies")
+  .addEventListener("click", () => handleCookieChoice("accept"));
+document
+  .getElementById("reject-cookies")
+  .addEventListener("click", () => handleCookieChoice("reject"));
